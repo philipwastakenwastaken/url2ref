dac0fe78cd97ed77094e9a2ab211a88bcd347e57
Philip Tofte Nielsen <s174274@student.dtu.dk>
2023-07-16 02:39:35 +0200

    Added tooltip for copy reference button

--- flaskapp/static/assets/custom.js
+++ flaskapp/static/assets/custom.js
@@ -1,4 +1,7 @@
 function copyToClipboard(msg) {
+    const tooltip = bootstrap.Tooltip.getInstance('#clipboard-button') // Returns a Bootstrap tooltip instance
+    // setContent example
+    tooltip.setContent({ '.tooltip-inner': 'Copied!' })
     navigator.clipboard.writeText(msg)
 }
 
@@ -10,3 +13,16 @@ document.getElementById('bd-theme').addEventListener('click',()=>{
         document.documentElement.setAttribute('data-bs-theme','dark')
     }
 })
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+const tooltipEl = document.getElementById('clipboard-button') // Returns a Bootstrap tooltip instance
+const tooltip = bootstrap.Tooltip.getOrCreateInstance(tooltipEl)
+tooltipEl.addEventListener('hidden.bs.tooltip', () => {
+    tooltip.setContent({ '.tooltip-inner': 'Copy to clipboard' })
+})
+
+tooltip.hide()
